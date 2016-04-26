80c836fe3d6d15b7cdf3965fc89f3a2c1eaa6f6d
cocopon <cocopon@me.com>
2016-04-26 16:38:19 +0900

    Fix test

--- test/constraint/test_max_number_constraint.js
+++ test/constraint/test_max_number_constraint.js
@@ -1,47 +1,17 @@
 const MaxNumberConstraint = require('../../src/main/js/constraint/max_number_constraint');
-const Constraint          = require('../../src/main/js/model/model');
 
 describe('MaxNumberConstraint', () => {
-	it('should not constrain value by default', () => {
-		const c = new MaxNumberConstraint();
-
-		assert.strictEqual(c.format(-273.15), -273.15);
-		assert.strictEqual(c.format(3.1416), 3.1416);
-	});
-
-	it('should have null value by default', () => {
-		const c = new MaxNumberConstraint();
-
-		assert.isNull(c.getMaxValue());
-	});
-
 	it('should set value', () => {
-		const c = new MaxNumberConstraint();
+		const c = new MaxNumberConstraint(3.1416);
 
-		c.setMaxValue(3.1416);
 		assert.strictEqual(c.getMaxValue(), 3.1416);
 	});
 
-	it('should fire change event', () => {
-		const c = new MaxNumberConstraint();
-
-		const spy = sinon.spy();
-		c.getEmitter().on(
-			Constraint.EVENT_CHANGE,
-			spy
-		);
-		c.setMaxValue(100);
-
-		assert.isTrue(spy.calledOnce);
-	});
-
 	it('should constraint value', () => {
-		const c = new MaxNumberConstraint();
+		const c = new MaxNumberConstraint(100.0);
 
-		c.setMaxValue(100.0);
-
-		assert.strictEqual(c.format(99.9), 99.9);
-		assert.strictEqual(c.format(100.0), 100.0);
-		assert.strictEqual(c.format(100.1), 100.0);
+		assert.strictEqual(c.constrain(99.9), 99.9);
+		assert.strictEqual(c.constrain(100.0), 100.0);
+		assert.strictEqual(c.constrain(100.1), 100.0);
 	});
 });
--- test/constraint/test_step_number_constraint.js
+++ test/constraint/test_step_number_constraint.js
@@ -1,47 +1,17 @@
 const StepNumberConstraint = require('../../src/main/js/constraint/step_number_constraint');
-const Constraint           = require('../../src/main/js/model/model');
 
 describe('StepNumberConstraint', () => {
-	it('should not constrain value by default', () => {
-		const c = new StepNumberConstraint();
-
-		assert.strictEqual(c.format(-273.15), -273.15);
-		assert.strictEqual(c.format(3.1416), 3.1416);
-	});
-
-	it('should have null value by default', () => {
-		const c = new StepNumberConstraint();
-
-		assert.isNull(c.getStepValue());
-	});
-
 	it('should set value', () => {
-		const c = new StepNumberConstraint();
+		const c = new StepNumberConstraint(10);
 
-		c.setStepValue(10);
 		assert.strictEqual(c.getStepValue(), 10);
 	});
 
-	it('should fire change event', () => {
-		const c = new StepNumberConstraint();
-
-		const spy = sinon.spy();
-		c.getEmitter().on(
-			Constraint.EVENT_CHANGE,
-			spy
-		);
-		c.setStepValue(100);
-
-		assert.isTrue(spy.calledOnce);
-	});
-
 	it('should constraint value', () => {
-		const c = new StepNumberConstraint();
+		const c = new StepNumberConstraint(10);
 
-		c.setStepValue(10);
-
-		assert.strictEqual(c.format(99), 90);
-		assert.strictEqual(c.format(100), 100);
-		assert.strictEqual(c.format(101), 100);
+		assert.strictEqual(c.constrain(99), 90);
+		assert.strictEqual(c.constrain(100), 100);
+		assert.strictEqual(c.constrain(101), 100);
 	});
 });
