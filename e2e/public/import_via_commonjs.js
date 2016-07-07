50756568c76ae206c8d57096ef31cce48eff0e8f
Kuniwak <yuki.kokubun@mixi.co.jp>
2016-07-07 23:26:44 +0900

    Start E2E testing

---
+++ e2e/public/import_via_commonjs.js
@@ -0,0 +1,20 @@
+/* global require */
+var Tweakpane = require('tweakpane');
+
+var observable = {
+  x: 0,
+};
+
+setInterval(function() {
+  var progress = (Date.now() % 1000) / 1000;
+  observable.x = Math.sin(progress * 2 * Math.PI);
+}, 100);
+
+var pane = new Tweakpane();
+var folder = pane.addFolder('Graph');
+folder.addGraph(observable, 'x', {
+  label: 'sin',
+  count: 50,
+  min: -1.0,
+  max: 1.0
+});
