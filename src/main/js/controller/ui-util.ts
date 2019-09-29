6f7aa0805fc170b116e6824e290dc8b2f9f485cf
cocopon <cocopon@me.com>
2019-09-29 10:26:36 +0900

    Fix bug of unwanted hue changing in color picker

--- src/main/js/controller/ui-util.ts
+++ src/main/js/controller/ui-util.ts
@@ -1,6 +1,8 @@
+import {Constraint} from '../constraint/constraint';
+import StepConstraint from '../constraint/step';
+import ConstraintUtil from '../constraint/util';
+import TypeUtil, {Class} from '../misc/type-util';
 import FolderController from './folder';
-
-import {Class} from '../misc/type-util';
 import {UiController} from './ui';
 
 /**
@@ -27,4 +29,45 @@ export function findControllers<Controller>(
 		},
 		[] as Controller[],
 	);
+}
+
+function findStep(constraint: Constraint<number> | undefined): number | null {
+	const c = constraint
+		? ConstraintUtil.findConstraint(constraint, StepConstraint)
+		: null;
+	if (!c) {
+		return null;
+	}
+
+	return c.step;
+}
+
+/**
+ * @hidden
+ */
+export function getStepForTextInput(
+	constraint: Constraint<number> | undefined,
+): number {
+	const step = findStep(constraint);
+	return TypeUtil.getOrDefault<number>(step, 1);
+}
+
+interface StepKeys {
+	altKey: boolean;
+	keyCode: number;
+	shiftKey: boolean;
+}
+
+/**
+ * @hidden
+ */
+export function getStepForKey(baseStep: number, keys: StepKeys): number {
+	const step = baseStep * (keys.altKey ? 0.1 : 1) * (keys.shiftKey ? 10 : 1);
+
+	if (keys.keyCode === 38) {
+		return +step;
+	} else if (keys.keyCode === 40) {
+		return -step;
+	}
+	return 0;
 }
---
+++ src/main/js/view/input/rgb-text.ts
@@ -0,0 +1,100 @@
+import {Formatter} from '../../formatter/formatter';
+import ClassName from '../../misc/class-name';
+import * as DisposingUtil from '../../misc/disposing-util';
+import PaneError from '../../misc/pane-error';
+import Color from '../../model/color';
+import InputValue from '../../model/input-value';
+import View from '../view';
+import {InputView} from './input';
+
+interface Config {
+	formatter: Formatter<number>;
+	value: InputValue<Color>;
+}
+
+const COMPONENT_LABELS: [string, string, string] = ['R', 'G', 'B'];
+const className = ClassName('rgbtxt', 'input');
+
+/**
+ * @hidden
+ */
+export default class RgbTextInputView extends View implements InputView<Color> {
+	public readonly value: InputValue<Color>;
+	private formatter_: Formatter<number>;
+	private inputElems_:
+		| [HTMLInputElement, HTMLInputElement, HTMLInputElement]
+		| null;
+
+	constructor(document: Document, config: Config) {
+		super(document);
+
+		this.onValueChange_ = this.onValueChange_.bind(this);
+
+		this.formatter_ = config.formatter;
+
+		this.element.classList.add(className());
+
+		const labelElems = COMPONENT_LABELS.map((text) => {
+			const elem = document.createElement('label');
+			elem.classList.add(className('l'));
+			elem.textContent = text;
+			return elem;
+		});
+		const inputElems = COMPONENT_LABELS.map(() => {
+			const inputElem = document.createElement('input');
+			inputElem.classList.add(className('i'));
+			inputElem.type = 'text';
+			return inputElem;
+		});
+		COMPONENT_LABELS.forEach((_, index) => {
+			const elem = document.createElement('div');
+			elem.classList.add(className('w'));
+			elem.appendChild(labelElems[index]);
+			elem.appendChild(inputElems[index]);
+			this.element.appendChild(elem);
+		});
+
+		this.inputElems_ = [inputElems[0], inputElems[1], inputElems[2]];
+
+		config.value.emitter.on('change', this.onValueChange_);
+		this.value = config.value;
+
+		this.update();
+	}
+
+	public dispose(): void {
+		if (this.inputElems_) {
+			this.inputElems_.forEach((elem) => {
+				DisposingUtil.disposeElement(elem);
+			});
+			this.inputElems_ = null;
+		}
+
+		super.dispose();
+	}
+
+	get inputElements(): [HTMLInputElement, HTMLInputElement, HTMLInputElement] {
+		if (!this.inputElems_) {
+			throw PaneError.alreadyDisposed();
+		}
+		return this.inputElems_;
+	}
+
+	public update(): void {
+		const inputElems = this.inputElems_;
+		if (!inputElems) {
+			throw PaneError.alreadyDisposed();
+		}
+
+		const rgbComps = this.value.rawValue.getComponents('rgb');
+
+		rgbComps.forEach((comp, index) => {
+			const inputElem = inputElems[index];
+			inputElem.value = this.formatter_.format(comp);
+		});
+	}
+
+	private onValueChange_(): void {
+		this.update();
+	}
+}
