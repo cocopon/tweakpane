c517f85ea6a0f925183f182a7f4b3b8624c577c1
cocopon <cocopon@me.com>
2019-11-11 14:47:53 +0900

    Add test cases, #19

--- src/main/js/formatter/number-test.ts
+++ src/main/js/formatter/number-test.ts
@@ -9,9 +9,45 @@ describe(NumberFormatter.name, () => {
 		assert.strictEqual(f.digits, 3);
 	});
 
-	it('should format number', () => {
-		assert.strictEqual(new NumberFormatter(2).format(0), '0.00');
-		assert.strictEqual(new NumberFormatter(0).format(3.14), '3');
-		assert.strictEqual(new NumberFormatter(2).format(141.41356), '141.41');
+	[
+		{
+			expected: '0.00',
+			params: {
+				digits: 2,
+				value: 0,
+			},
+		},
+		{
+			expected: '3',
+			params: {
+				digits: 0,
+				value: 3.14,
+			},
+		},
+		{
+			expected: '141.41',
+			params: {
+				digits: 2,
+				value: 141.41356,
+			},
+		},
+	].forEach((testCase) => {
+		context(`when ${JSON.stringify(testCase.params)}`, () => {
+			it(`it should format to ${JSON.stringify(testCase.expected)}`, () => {
+				const f = new NumberFormatter(testCase.params.digits);
+				assert.strictEqual(f.format(testCase.params.value), testCase.expected);
+			});
+		});
+	});
+
+	[-10, 0, 20, 100, 1000].forEach((digits) => {
+		context(`when ${digits}`, () => {
+			it(`it should format without error`, () => {
+				const f = new NumberFormatter(digits);
+				assert.doesNotThrow(() => {
+					f.format(Math.PI);
+				});
+			});
+		});
 	});
 });
