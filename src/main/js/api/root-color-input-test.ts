a5e7d8d97ddb53db254f649ef7b80d24f2d9d1a6
cocopon <cocopon@me.com>
2020-06-15 19:55:31 +0900

    Add more test cases

--- src/main/js/api/root-color-input-test.ts
+++ src/main/js/api/root-color-input-test.ts
@@ -12,8 +12,8 @@ import {ColorSwatchTextInputView} from '../view/input/color-swatch-text';
 import {RootApi} from './root';
 import {InputParams} from './types';
 
-function createApi(): RootApi {
-	const c = new RootController(TestUtil.createWindow().document, {
+function createApi(win: Window): RootApi {
+	const c = new RootController(win.document, {
 		viewModel: new ViewModel(),
 	});
 	return new RootApi(c);
@@ -67,7 +67,7 @@ describe(RootApi.name, () => {
 	testCases.forEach((testCase) => {
 		context(`when params = ${JSON.stringify(testCase.params)}`, () => {
 			it(`should return class ${testCase.expectedClass.name}`, () => {
-				const api = createApi();
+				const api = createApi(TestUtil.createWindow());
 				const obj = {foo: testCase.value};
 				const bapi = api.addInput(obj, 'foo', testCase.params);
 				assert.instanceOf(bapi.controller.controller, testCase.expectedClass);
@@ -95,7 +95,7 @@ describe(RootApi.name, () => {
 	].forEach((testCase) => {
 		context(`when params = ${JSON.stringify(testCase.params)}`, () => {
 			it(`should have right input value ${testCase.expected.inputValue}`, () => {
-				const api = createApi();
+				const api = createApi(TestUtil.createWindow());
 				const obj = {foo: testCase.params.input};
 				const bapi = api.addInput(obj, 'foo');
 
@@ -106,6 +106,82 @@ describe(RootApi.name, () => {
 
 				const inputElem = view.textInputView.inputElement;
 				assert.equal(inputElem.value, testCase.expected.inputValue);
+			});
+		});
+	});
+
+	[
+		{
+			expected: {
+				initialInputText: '#112233',
+				updatedValue: '#445566',
+			},
+			params: {
+				input: '#123',
+				updatedText: '#456',
+			},
+		},
+		{
+			expected: {
+				initialInputText: '#445566',
+				updatedValue: '#778899',
+			},
+			params: {
+				input: '#445566',
+				updatedText: '#778899',
+			},
+		},
+		{
+			expected: {
+				initialInputText: '#12345678',
+				updatedValue: '#001122ff',
+			},
+			params: {
+				input: '#12345678',
+				updatedText: '#012',
+			},
+		},
+		{
+			expected: {
+				initialInputText: 'rgb(255, 255, 0)',
+				updatedValue: 'rgb(0, 127, 255)',
+			},
+			params: {
+				input: 'rgb(255,255,0)',
+				updatedText: 'rgb(0,127,255)',
+			},
+		},
+		{
+			expected: {
+				initialInputText: 'rgba(12, 34, 56, 0.70)',
+				updatedValue: 'rgba(89, 10, 12, 0.34)',
+			},
+			params: {
+				input: 'rgba(12,34,56,0.7)',
+				updatedText: 'rgba(89,10,12,0.34)',
+			},
+		},
+	].forEach(({expected, params}) => {
+		context(`when params = ${JSON.stringify(params)}`, () => {
+			const win = TestUtil.createWindow();
+			const api = createApi(win);
+			const obj = {foo: params.input};
+			const bapi = api.addInput(obj, 'foo');
+
+			const view = bapi.controller.controller.view;
+			if (!(view instanceof ColorSwatchTextInputView)) {
+				throw new Error('Unexpected view');
+			}
+			const inputElem = view.textInputView.inputElement;
+
+			it(`should have initial input value ${expected.initialInputText}`, () => {
+				assert.equal(inputElem.value, expected.initialInputText);
+			});
+
+			it(`should have updated value ${expected.updatedValue}`, () => {
+				inputElem.value = params.updatedText;
+				inputElem.dispatchEvent(TestUtil.createEvent(win, 'change'));
+				assert.strictEqual(obj.foo, expected.updatedValue);
 			});
 		});
 	});
