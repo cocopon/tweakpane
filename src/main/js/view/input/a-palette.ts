8d4eaee92ba5452a1857ab71cee00e5be825a5f7
cocopon <cocopon@me.com>
2020-10-29 19:19:46 +0900

    Replace alpha gradient with CSS

--- src/main/js/view/input/a-palette.ts
+++ src/main/js/view/input/a-palette.ts
@@ -1,7 +1,6 @@
 import * as ColorConverter from '../../converter/color';
 import {ClassName} from '../../misc/class-name';
 import * as DisposingUtil from '../../misc/disposing-util';
-import * as DomUtil from '../../misc/dom-util';
 import {NumberUtil} from '../../misc/number-util';
 import {PaneError} from '../../misc/pane-error';
 import {Color} from '../../model/color';
@@ -14,14 +13,12 @@ interface Config extends ViewConfig {
 	value: InputValue<Color>;
 }
 
-const CANVAS_RESOL = 64;
-
 /**
  * @hidden
  */
 export class APaletteInputView extends View {
 	public readonly value: InputValue<Color>;
-	private canvasElem_: HTMLCanvasElement | null;
+	private colorElem_: HTMLDivElement | null;
 	private markerElem_: HTMLDivElement | null;
 	private previewElem_: HTMLDivElement | null;
 
@@ -36,12 +33,14 @@ export class APaletteInputView extends View {
 		this.element.classList.add(className());
 		this.element.tabIndex = 0;
 
-		const canvasElem = document.createElement('canvas');
-		canvasElem.height = 1;
-		canvasElem.width = CANVAS_RESOL;
-		canvasElem.classList.add(className('c'));
-		this.element.appendChild(canvasElem);
-		this.canvasElem_ = canvasElem;
+		const barElem = document.createElement('div');
+		barElem.classList.add(className('b'));
+		this.element.appendChild(barElem);
+
+		const colorElem = document.createElement('div');
+		colorElem.classList.add(className('c'));
+		barElem.appendChild(colorElem);
+		this.colorElem_ = colorElem;
 
 		const markerElem = document.createElement('div');
 		markerElem.classList.add(className('m'));
@@ -56,46 +55,34 @@ export class APaletteInputView extends View {
 		this.update();
 
 		config.model.emitter.on('dispose', () => {
-			this.canvasElem_ = DisposingUtil.disposeElement(this.canvasElem_);
+			this.colorElem_ = DisposingUtil.disposeElement(this.colorElem_);
 			this.markerElem_ = DisposingUtil.disposeElement(this.markerElem_);
 		});
 	}
 
-	get canvasElement(): HTMLCanvasElement {
-		if (!this.canvasElem_) {
-			throw PaneError.alreadyDisposed();
-		}
-		return this.canvasElem_;
-	}
-
 	public update(): void {
-		if (!this.markerElem_ || !this.previewElem_) {
+		if (!this.markerElem_ || !this.previewElem_ || !this.colorElem_) {
 			throw PaneError.alreadyDisposed();
 		}
-
-		const ctx = DomUtil.getCanvasContext(this.canvasElement);
-		if (!ctx) {
-			return;
-		}
-
-		const width = this.canvasElement.width;
-		const height = this.canvasElement.height;
-		const imgData = ctx.getImageData(0, 0, width, height);
-		const data = imgData.data;
-		ctx.clearRect(0, 0, width, height);
 
 		const c = this.value.rawValue;
 		const rgbaComps = c.getComponents('rgb');
-
-		for (let ix = 0; ix < width; ix++) {
-			const alpha = NumberUtil.map(ix, 0, width, 0, 255);
-			const i = ix * 4;
-			data[i] = rgbaComps[0];
-			data[i + 1] = rgbaComps[1];
-			data[i + 2] = rgbaComps[2];
-			data[i + 3] = alpha;
-		}
-		ctx.putImageData(imgData, 0, 0);
+		const leftColor = new Color(
+			[rgbaComps[0], rgbaComps[1], rgbaComps[2], 0],
+			'rgb',
+		);
+		const rightColor = new Color(
+			[rgbaComps[0], rgbaComps[1], rgbaComps[2], 255],
+			'rgb',
+		);
+		const gradientComps = [
+			'to right',
+			ColorConverter.toFunctionalRgbaString(leftColor),
+			ColorConverter.toFunctionalRgbaString(rightColor),
+		];
+		this.colorElem_.style.background = `linear-gradient(${gradientComps.join(
+			',',
+		)})`;
 
 		this.previewElem_.style.backgroundColor = ColorConverter.toFunctionalRgbaString(
 			c,
