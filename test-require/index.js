161a5a9584eb9440faac4a4c7ff4b9da2f5baa1b
cocopon <cocopon@me.com>
2021-02-11 17:28:35 +0900

    Make module resolution testing more strict

--- test-require/index.js
+++ test-require/index.js
@@ -1,17 +1,24 @@
 'use strict';
 
 const Jsdom = require('jsdom').JSDOM;
+
+// Require the default module
 const Tweakpane = require('tweakpane');
 
 const PARAMS = {
 	foo: 1,
 };
+
+// Creating a pane
 const pane = new Tweakpane({
 	document: (new Jsdom('')).window.document,
 });
+
+// Add an input
 pane.addInput(PARAMS, 'foo', {
 	max: 1,
 	min: 0,
 	step: 1,
 });
+
 console.log(pane);
