89b85bd9268d04c4042ae78c8de92c39cc6ca257
cocopon <cocopon@me.com>
2021-03-14 11:25:38 +0900

    Apply synchronization with constraint of primary value

---
+++ lib/plugin/common/model/value-sync-test.ts
@@ -0,0 +1,62 @@
+import {assert} from 'chai';
+import {describe, it} from 'mocha';
+
+import {Point2d} from '../../input-bindings/point-2d/model/point-2d';
+import {Constraint} from '../constraint/constraint';
+import {Value} from './value';
+import {connectValues} from './value-sync';
+
+describe(connectValues.name, () => {
+	it('should set initial value', () => {
+		const pv = new Value(new Point2d(1, 2));
+		const sv = new Value(0);
+		connectValues({
+			primary: pv,
+			secondary: sv,
+
+			forward: (p) => {
+				return p.rawValue.x;
+			},
+			backward: (p, s) => {
+				const comps = p.rawValue.getComponents();
+				comps[0] = s.rawValue;
+				return new Point2d(...comps);
+			},
+		});
+		assert.strictEqual(sv.rawValue, 1);
+	});
+
+	it('should apply constraint of primary value', () => {
+		class TestConstraint implements Constraint<Point2d> {
+			constrain(): Point2d {
+				// Secondary value will be changed by constraint of primary value
+				return new Point2d(-1, -1);
+			}
+		}
+
+		const pv = new Value(new Point2d(1, 2), {
+			constraint: new TestConstraint(),
+		});
+
+		const sv = new Value(0);
+		connectValues({
+			primary: pv,
+			secondary: sv,
+
+			forward: (p) => {
+				return p.rawValue.x;
+			},
+			backward: (p, s) => {
+				const comps = p.rawValue.getComponents();
+				comps[0] = s.rawValue;
+				return new Point2d(...comps);
+			},
+		});
+
+		// Try to change secondary value
+		sv.rawValue = 10;
+		// ...and it should be updated by primary constraint
+
+		assert.strictEqual(sv.rawValue, -1);
+	});
+});
