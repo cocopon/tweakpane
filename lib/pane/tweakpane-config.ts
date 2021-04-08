7897a08598fd526fefd60b610f673114254d527c
cocopon <cocopon@me.com>
2021-04-09 06:49:24 +0900

    Fix API document

--- lib/pane/tweakpane-config.ts
+++ lib/pane/tweakpane-config.ts
@@ -1,6 +1,15 @@
 export interface TweakpaneConfig {
+	/**
+	 * The custom container element of the pane.
+	 */
 	container?: HTMLElement;
+	/**
+	 * The default expansion of the pane.
+	 */
 	expanded?: boolean;
+	/**
+	 * The pane title that can expand/collapse the entire pane.
+	 */
 	title?: string;
 
 	/**
