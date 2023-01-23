dd8db1d8eabc5025c70b0f8885e5159b76de96e9
cocopon <cocopon@me.com>
2023-01-23 17:37:54 +0900

    Add effectiveDisabled to ViewProps

--- packages/core/src/common/model/view-props-test.ts
+++ packages/core/src/common/model/view-props-test.ts
@@ -1,43 +1,52 @@
 import * as assert from 'assert';
 import {describe, it as context, it} from 'mocha';
 
-import {ViewProps} from './view-props';
+import {ViewProps, ViewPropsObject} from './view-props';
 
 describe(ViewProps.name, () => {
-	[
-		{
-			params: undefined,
-			expected: {
-				disabled: false,
-				disposed: false,
-				hidden: false,
+	(
+		[
+			{
+				params: undefined,
+				expected: {
+					disabled: false,
+					disposed: false,
+					hidden: false,
+				},
 			},
-		},
-		{
-			params: {},
-			expected: {
-				disabled: false,
-				disposed: false,
-				hidden: false,
+			{
+				params: {},
+				expected: {
+					disabled: false,
+					disposed: false,
+					hidden: false,
+				},
 			},
-		},
-		{
-			params: {disabled: true},
-			expected: {
-				disabled: true,
-				disposed: false,
-				hidden: false,
+			{
+				params: {disabled: true},
+				expected: {
+					disabled: true,
+					disposed: false,
+					hidden: false,
+				},
 			},
-		},
-		{
-			params: {hidden: true},
-			expected: {
-				disabled: false,
-				disposed: false,
-				hidden: true,
+			{
+				params: {hidden: true},
+				expected: {
+					disabled: false,
+					disposed: false,
+					hidden: true,
+				},
 			},
-		},
-	].forEach(({params, expected}) => {
+		] as {
+			params: Partial<ViewPropsObject>;
+			expected: {
+				disabled: boolean;
+				disposed: boolean;
+				hidden: boolean;
+			};
+		}[]
+	).forEach(({params, expected}) => {
 		context(`when ${JSON.stringify(params)}`, () => {
 			it('should set initial value', () => {
 				const p = ViewProps.create(params);
@@ -46,5 +55,90 @@ describe(ViewProps.name, () => {
 				assert.strictEqual(p.get('hidden'), expected.hidden);
 			});
 		});
+	});
+
+	[
+		{
+			params: {
+				disabled: false,
+				parentDisabled: null,
+			},
+			expected: false,
+		},
+		{
+			params: {
+				disabled: true,
+				parentDisabled: null,
+			},
+			expected: true,
+		},
+		{
+			params: {
+				disabled: false,
+				parentDisabled: false,
+			},
+			expected: false,
+		},
+		{
+			params: {
+				disabled: false,
+				parentDisabled: true,
+			},
+			expected: true,
+		},
+		{
+			params: {
+				disabled: true,
+				parentDisabled: false,
+			},
+			expected: true,
+		},
+		{
+			params: {
+				disabled: true,
+				parentDisabled: true,
+			},
+			expected: true,
+		},
+	].forEach(({params, expected}) => {
+		context(`when ${JSON.stringify(params)}`, () => {
+			it('should update effectiveDisabled', () => {
+				const p = ViewProps.create({
+					disabled: params.disabled,
+				});
+				p.set(
+					'parent',
+					params.parentDisabled !== null
+						? ViewProps.create({
+								disabled: params.parentDisabled,
+						  })
+						: null,
+				);
+				assert.strictEqual(p.get('disabled'), expected);
+			});
+		});
+	});
+
+	it('should apply parent effectiveDisabled to child', () => {
+		const p = ViewProps.create({
+			disabled: false,
+			parent: ViewProps.create({
+				disabled: false,
+				parent: ViewProps.create({disabled: true}),
+			}),
+		});
+		assert.strictEqual(p.effectiveDisabled.rawValue, true);
+	});
+
+	it('should apply parent updates', () => {
+		const p = ViewProps.create({
+			disabled: false,
+			parent: ViewProps.create({
+				disabled: false,
+			}),
+		});
+
+		p.get('parent')?.set('disabled', true);
+		assert.strictEqual(p.effectiveDisabled.rawValue, true);
 	});
 });
--- packages/core/src/common/model/view-props.ts
+++ packages/core/src/common/model/view-props.ts
@@ -1,13 +1,16 @@
 import {ClassName} from '../view/class-name';
 import {valueToClassName} from '../view/reactive';
 import {bindValueMap} from './reactive';
-import {Value} from './value';
+import {ReadonlyValue, SetRawValue} from './readonly-value';
+import {Value, ValueEvents} from './value';
 import {ValueMap, ValueMapEvents} from './value-map';
+import {createValue} from './values';
 
 export type ViewPropsObject = {
 	disabled: boolean;
 	disposed: boolean;
 	hidden: boolean;
+	parent: ViewProps | null;
 };
 
 export type ViewPropsEvents = ValueMapEvents<ViewPropsObject>;
@@ -25,21 +28,44 @@ interface Disableable {
 }
 
 export class ViewProps extends ValueMap<ViewPropsObject> {
+	private readonly effectiveDisabled_: ReadonlyValue<boolean>;
+	private readonly setEffectiveDisabled_: SetRawValue<boolean>;
+
 	constructor(valueMap: {
 		[Key in keyof ViewPropsObject]: Value<ViewPropsObject[Key]>;
 	}) {
 		super(valueMap);
+
+		this.onDisabledChange_ = this.onDisabledChange_.bind(this);
+		this.onParentChange_ = this.onParentChange_.bind(this);
+		this.onParentEffectiveDisabledChange_ =
+			this.onParentEffectiveDisabledChange_.bind(this);
+
+		[this.effectiveDisabled_, this.setEffectiveDisabled_] =
+			ReadonlyValue.create(createValue(this.getEffectiveDisabled_()));
+
+		this.value('disabled').emitter.on('change', this.onDisabledChange_);
+		this.value('parent').emitter.on('change', this.onParentChange_);
+		this.get('parent')?.effectiveDisabled.emitter.on(
+			'change',
+			this.onParentEffectiveDisabledChange_,
+		);
 	}
 
 	public static create(opt_initialValue?: Partial<ViewPropsObject>): ViewProps {
 		const initialValue: Partial<ViewPropsObject> = opt_initialValue ?? {};
-		const coreObj = {
-			disabled: initialValue.disabled ?? false,
-			disposed: false,
-			hidden: initialValue.hidden ?? false,
-		};
-		const core = ValueMap.createCore(coreObj);
-		return new ViewProps(core);
+		return new ViewProps(
+			ValueMap.createCore<ViewPropsObject>({
+				disabled: initialValue.disabled ?? false,
+				disposed: false,
+				hidden: initialValue.hidden ?? false,
+				parent: initialValue.parent ?? null,
+			}),
+		);
+	}
+
+	get effectiveDisabled(): ReadonlyValue<boolean> {
+		return this.effectiveDisabled_;
 	}
 
 	public bindClassModifiers(elem: HTMLElement): void {
@@ -48,8 +74,8 @@ export class ViewProps extends ValueMap<ViewPropsObject> {
 	}
 
 	public bindDisabled(target: Disableable): void {
-		bindValueMap(this, 'disabled', (disabled: boolean) => {
-			target.disabled = disabled;
+		this.effectiveDisabled_.emitter.on('change', (ev) => {
+			target.disabled = ev.rawValue;
 		});
 	}
 
@@ -65,5 +91,41 @@ export class ViewProps extends ValueMap<ViewPropsObject> {
 				callback();
 			}
 		});
+	}
+
+	/**
+	 * Gets an effective disabled of the view.
+	 * Disabled of the view will be affected by its disabled and its parent disabled.
+	 */
+	private getEffectiveDisabled_(): boolean {
+		const parent = this.get('parent');
+		const parentDisabled = parent ? parent.effectiveDisabled.rawValue : false;
+		return parentDisabled || this.get('disabled');
+	}
+
+	private updateEffectiveDisabled_(): void {
+		this.setEffectiveDisabled_(this.getEffectiveDisabled_());
+	}
+
+	private onDisabledChange_(): void {
+		this.updateEffectiveDisabled_();
+	}
+
+	private onParentEffectiveDisabledChange_(): void {
+		this.updateEffectiveDisabled_();
+	}
+
+	private onParentChange_(ev: ValueEvents<ViewProps | null>['change']): void {
+		const prevParent = ev.previousRawValue;
+		prevParent?.effectiveDisabled.emitter.off(
+			'change',
+			this.onParentEffectiveDisabledChange_,
+		);
+		this.get('parent')?.effectiveDisabled.emitter.on(
+			'change',
+			this.onParentEffectiveDisabledChange_,
+		);
+
+		this.updateEffectiveDisabled_();
 	}
 }
