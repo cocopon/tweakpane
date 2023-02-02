4a9db42a397c49704c4f253a8562551aaac49647
cocopon <cocopon@me.com>
2023-02-02 23:39:49 +0900

    Remove unused type

--- packages/core/src/blade/binding/api/monitor-binding.ts
+++ packages/core/src/blade/binding/api/monitor-binding.ts
@@ -1,13 +1,6 @@
 import {TpBuffer} from '../../../common/model/buffered-value';
-import {TpChangeEvent} from '../../common/api/tp-event';
 import {MonitorBindingController} from '../controller/monitor-binding';
 import {BindingApi} from './binding';
-
-export interface MonitorBindingApiEvents<T> {
-	change: {
-		event: TpChangeEvent<T>;
-	};
-}
 
 /**
  * The API for the monitor binding between the parameter and the pane.
