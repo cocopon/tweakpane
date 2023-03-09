26f5cf6e683fd048fc0efe154c41503b19d3aff7
cocopon <cocopon@me.com>
2023-03-10 00:44:05 +0900

    Refactor refresh(), #510

--- packages/tweakpane/src/main/ts/blade/root/api/root.ts
+++ packages/tweakpane/src/main/ts/blade/root/api/root.ts
@@ -1,25 +1,6 @@
-import {
-	BindingValue,
-	FolderApi,
-	isInputBindingController,
-	isMonitorBindingController,
-	MonitorBindingValue,
-	PluginPool,
-	Rack,
-} from '@tweakpane/core';
+import {FolderApi, PluginPool} from '@tweakpane/core';
 
 import {RootController} from '../controller/root';
-
-function findInputBindingValues(rack: Rack): BindingValue<unknown>[] {
-	const vcs = rack.find(isInputBindingController);
-	return vcs.map((vc) => vc.value);
-}
-
-function findMonitorBindingValues(rack: Rack): MonitorBindingValue<unknown>[] {
-	return rack
-		.find(isMonitorBindingController)
-		.map((vc) => vc.value as MonitorBindingValue<unknown>);
-}
 
 export class RootApi extends FolderApi {
 	/**
@@ -31,22 +12,5 @@ export class RootApi extends FolderApi {
 
 	get element(): HTMLElement {
 		return this.controller_.view.element;
-	}
-
-	/**
-	 * Refreshes all bindings of the pane.
-	 */
-	public refresh(): void {
-		// Force-read all input bindings
-		findInputBindingValues(this.controller_.rackController.rack).forEach((bv) =>
-			bv.fetch(),
-		);
-
-		// Force-read all monitor bindings
-		findMonitorBindingValues(this.controller_.rackController.rack).forEach(
-			(mbv) => {
-				mbv.fetch();
-			},
-		);
 	}
 }
